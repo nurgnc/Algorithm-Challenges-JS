4019c8cd7ec44a814aabcb3fd052688c16b3224f
nurgnc <gencbedriyenur@gmail.com>
2022-03-19 00:27:41 +0300

    new katas

---
+++ 7kyu Solutions/Lost number in number sequence.js
@@ -0,0 +1,49 @@
+/*
+An ordered sequence of numbers from 1 to N is given. One number might have deleted from it, then the remaining numbers were mixed. Find the number that was deleted.
+
+Example:
+
+The starting array sequence is [1,2,3,4,5,6,7,8,9]
+The mixed array with one deleted number is [3,2,4,6,7,8,1,9]
+Your function should return the int 5.
+If no number was deleted from the starting array, your function should return the int 0.
+
+Note: N may be 1 or less (in the latter case, the first array will be []).
+*/
+
+// my solution
+function findDeletedNumber(arr, mixArr) {
+  return Math.abs(
+    arr.reduce((a, b) => a + b, 0) - mixArr.reduce((a, b) => a + b, 0)
+  );
+}
+
+// sample test
+describe("Tests", () => {
+  it("test", () => {
+    // These are some CW specific test methods available:
+    //    Test.expect(boolean, [optional] message)
+    //    Test.assertEquals(actual, expected, [optional] message)
+    //    Test.assertSimilar(actual, expected, [optional] message)
+    //    Test.assertNotEquals(actual, expected, [optional] message)
+
+    Test.assertEquals(
+      findDeletedNumber([1, 2, 3, 4, 5], [3, 4, 1, 5]),
+      2,
+      "Deletion"
+    );
+    Test.assertEquals(
+      findDeletedNumber([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 9, 7, 4, 6, 2, 3, 8]),
+      5,
+      "Deletion"
+    );
+    Test.assertEquals(
+      findDeletedNumber(
+        [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [5, 7, 6, 9, 4, 8, 1, 2, 3]
+      ),
+      0,
+      "No deletion"
+    );
+  });
+});
