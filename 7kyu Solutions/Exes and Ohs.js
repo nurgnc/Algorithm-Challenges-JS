324461f2063e35ae4cb28e4c24b2a6738faad4d5
Bedriye Nur GENÇ <gencbedriyenur@gmail.com>
2022-08-18 14:48:22 +0300

    Create Exes and Ohs.js

---
+++ 7kyu Solutions/Exes and Ohs.js
@@ -0,0 +1,30 @@
+/*
+Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false
+*/
+
+// my solution
+function XO(str) {
+  const oCount = str.split("").filter(item => item === "o" || item === "O").length
+  const xCount = str.split("").filter(item => item === "x" || item === "X").length
+  return (oCount === xCount)
+}
+
+// sample test
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(XO('xo'),true);
+Test.assertEquals(XO("xxOo"),true);
+Test.assertEquals(XO("xxxm"),false);
+Test.assertEquals(XO("Oo"),false);
+Test.assertEquals(XO("ooom"),false);
+  });
+});
+
